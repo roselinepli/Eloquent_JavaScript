72f81da37e626328601759f21cc80323ee5afc9e
Ping <roselinepli@gmail.com>
2023-06-22 19:50:28 -0400

    project3 finished

--- 15_Project3_A_Platform_Game.js
+++ 15_Project3_A_Platform_Game.js
@@ -352,3 +352,48 @@ function trackKeys(codes) {
     addEventListener("keyup", handler);
     return pressed;
 }
+
+function runAnimation(frameFunc) {
+    var lastTime = null;
+    function frame(time) {
+        var stop = false;
+        if (lastTime != null) {
+            var timeStep = Math.min(time - lastTime, 100) / 1000;
+            stop = frameFunc(timeStep) === false;
+        }
+        lastTime = time;
+        if (!stop)
+          requestAnimationFrame(frame);
+    }
+    requestAnimationFrame(frame);
+}
+
+var arrows = trackKeys(arrowCodes);
+
+function runLevel(level, Display, andThen) {
+    var display = new Display(document.body, level);
+    runAnimation(function(step) {
+        level.animate(step, arrows);
+        display.drawFrame(step);
+        if (level.isFinished()) {
+            display.clear();
+            if (andThen)
+              andThen(level.status);
+              return false;
+        }
+    });
+}
+
+function runGame(plans, Display) {
+    function startLevel(n) {
+        runLevel(new Level(plans[n]), Display, function(status) {
+            if (status == "lost")
+              startLevel(n);
+            else if (n < plans.length - 1)
+              startLevel(n + 1);
+            else
+              console.log("You win!");
+        });
+    }
+    startLevel(0);
+}
