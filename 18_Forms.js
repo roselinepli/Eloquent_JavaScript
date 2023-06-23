3e89aa4ccd2ef336dafe79033f768daf1de0b00a
Ping <roselinepli@gmail.com>
2023-06-23 10:56:10 -0400

    chapter18 finished

---
+++ 18_Forms.js
@@ -0,0 +1,13 @@
+// Fields
+
+<http>
+<p><input type="text" value = "abc">(text)</input></p>
+<p><input type="password" value = "abc">(password)</input></p> //Same as text but hids the text that is typed
+<p><input type="checkbox" checked>(checkbox)</input></p>
+<p><input type="radio" value = "A" name="choice"></input>
+   <input type="radio" value = "B" name="choice" checked></input>
+   <input type="radio" value = "C" name="choice"></input></p>
+<p><input type="file">(file)</input></p>
+</http>
+
+// Focus
